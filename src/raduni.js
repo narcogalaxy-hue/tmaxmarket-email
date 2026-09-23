44be931b8135b26f55aa16cf649091213efc7bfa
narcogalaxy-hue <narcogalaxy@gmail.com>
2026-09-23 03:27:51 +0200

    Update raduni.js

--- src/raduni.js
+++ src/raduni.js
@@ -189,6 +189,10 @@ async function createRadunoArticle({ titolo, gruppo, dataEvento, luogo, link, de
     metafields: [{ namespace: "custom", key: "data_evento", value: dataEvento, type: "date" }],
   };
 
+  if (imageUrl) {
+    articleInput.image = { url: imageUrl, altText: `Locandina: ${titolo}` };
+  }
+
   const data = await shopifyGraphQL(query, { article: articleInput });
   const result = data.articleCreate;
   if (result.userErrors && result.userErrors.length > 0) {
